9f1f10eb652052098c272817f702fd54c3a2ff56
macutko <matusgallik008@gmail.com>
2021-06-09 23:09:01 +0200

    fix: redact

--- server/config.js
+++ server/config.js
@@ -11,27 +11,27 @@ export class Config {
                 this.secret = process.env.SECRET;
                 this.baseURL = 'http://localhost:5001/';
                 this.connectionString =
-          'mongodb+srv://admin:TAGAdmin@server-v3-1-wnt86.mongodb.net/CONZLT_production?retryWrites=true&w=majority';
+          '';
             } else if (process.env.NODE_ENV === 'development') {
                 this.PORT = 12345;
                 this.baseURL = 'http://localhost:5001/';
                 this.mailConfig = {
                     service: 'gmail',
                     auth: {
-                        user: 'conzlttestmail@gmail.com',
-                        pass: 'conzlt123',
+                        user: '',
+                        pass: '',
                     },
                 };
                 this.mailBugOptions = {
-                    from: 'conzlttestmail@gmail.com',
+                    from: '',
                     to:
-            'incoming+macutko-tldl-client-22930948-issue-@incoming.gitlab.com',
+            '',
                 };
                 this.connectionString =
-          'mongodb+srv://admin:TAGAdmin@server-v3-1-wnt86.mongodb.net/CONZLT_dev?retryWrites=true&w=majority';
+          '';
                 this.stripe = require('stripe')(
                     // eslint-disable-next-line max-len
-                    'rk_test_51IDSTZECU7HrwjM1CctplX9fQXrLB635qNpBL6QpCyKXHQUXlMFyDCjbggdyvoqzhqBCthsR1rBbBDvIRjDVZXwK00BZo1zUxg'
+                    ''
                 );
                 this.secret =
           'THIS IS USED TO SIGN AND VERIFY JWT TOKENS, REPLACE IT WITH YOUR OWN SECRET, IT CAN BE ANY STRING';
